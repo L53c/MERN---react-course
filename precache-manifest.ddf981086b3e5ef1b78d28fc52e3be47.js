7c150ce1f014f0087eb8e0dc5c57ea4e057346d1
L53c <aurelio1978@gmail.com>
2020-07-06 04:40:09 +0100

    Add files via upload

---
+++ precache-manifest.ddf981086b3e5ef1b78d28fc52e3be47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1454db2137ec8f1f57bef1f37927206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a74b8bd5bcf05570d7bc",
+    "url": "/static/css/main.f0fb63ba.chunk.css"
+  },
+  {
+    "revision": "d5d09dab692727695176",
+    "url": "/static/js/2.203880f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.203880f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74b8bd5bcf05570d7bc",
+    "url": "/static/js/main.d4dee259.chunk.js"
+  },
+  {
+    "revision": "a40662e75da12101abdb",
+    "url": "/static/js/runtime-main.4ac1cc8c.js"
+  }
+]);
